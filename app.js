6f8f089de84a9fa45fbf2273b00e9bc94c669a7f
Aaron Claydon <claydon.aaron@gmail.com>
2015-06-15 17:12:32 +0100

    Initial framework

---
+++ app.js
@@ -0,0 +1,26 @@
+var browserify = require('browserify-middleware');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var app = express();
+
+// view engine setup
+app.set('views','views');
+app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }));
+app.set('view engine', '.hbs');
+
+//static files
+app.use(express.static(__dirname + '/public'));
+
+//bundle the main client side app
+app.get('/editor.js', browserify('./client/editor/main.js'));
+
+//static routes
+app.get('/', function(req, res){
+    res.render('index');
+});
+app.get('/editor', function(req, res){
+    res.render('editor');
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
