70abe2d5329e7a38f9ff406fc392adec5a0cc0d7
Aaron Claydon <claydon.aaron@gmail.com>
2015-06-16 22:36:20 +0100

    Loads blocks and displays names on components pane (last one message up)

---
+++ client/editor/componentPane.js
@@ -0,0 +1,19 @@
+var $ = require('jquery');
+
+//this is all very temporary
+module.exports.init = function(id, categories) {
+    var html = '';
+
+    for(var title in categories) {
+        html += '<strong>' + title + '</strong><br/>';
+
+        category = categories[title];
+        category.forEach(function(block) {
+            html += '- ' + block.identifier + '<br/>';
+        });
+
+        html += '<br/>';
+    }
+
+    $(id).html(html);
+}
